bc44e20aeae834b66f7f7152051cdbb6cd39dbd1
Lanix 08 <Lanix 08>
2015-04-28 13:26:17 -0500

    dos
    remplace index y acciones

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,17 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
-}); 
-});
 
+$('#btnvibrar').on ('tap', function() {
+	navigator.vibrate(1000);
+	
+	
+});  // tap btn_vibrar
+
+$('#btnbeep').on ('tap' , function() {
+	  navigator.beep(1);
+
+}); //tap btnbeep
+
+});
+});
